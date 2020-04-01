3bc4a98a3f4d37b856f3a477de661b2afe9d48d9
Rose <rose.gonoud@gmail.com>
2020-04-01 14:54:38 -0700

    all required work done

--- static/js/app.js
+++ static/js/app.js
@@ -1,15 +1,33 @@
 // This function is called when a dropdown menu item is selected
 function optionChanged(ID) {
+    // must fetch the data in order to access patient IDs in the onChange - github hosted, no server startup needed
     d3
     .json("https://raw.githubusercontent.com/rose-gonoud/plotly-challenge/master/data/samples.json")
     .then((samples) => {
 
-        arr = samples.names;
-        IDindex = arr.indexOf(ID);
+        // grab the index of the name selected from the data
+        var arr = samples.names;
+        var IDindex = arr.indexOf(ID);
         console.log(IDindex);
 
+        // grab the index of the metadata for selected patient
+        var meta = samples.metadata[IDindex];
+        console.log("meta", meta);
+
+        // clears the divs with the old patient's data in them, if it exists.
+        d3.select("#sample-metadata")
+        .selectAll("div").remove();
+
+        // grabs metadata obj for selected ID, uses Object.entries to seperate obj into key:value pairs, and appends to list to be added to page
+        for (let [key, value] of Object.entries(meta)) {
+            d3.select("#sample-metadata")
+            .append("div").text(`${key}: ${value}`);
+          };
+
+        // defines current patient based on ID selected during the change
         var patient = samples.samples[IDindex];
 
+        // loops through data associated with patient selected and throws sample_value, otu_id, and otu_label in one new list
         var barData = []
         for (var i=0; i < patient.sample_values.length; i++) {
             var barDatum = {"sample_value": patient.sample_values[i], "otu_id": patient.otu_ids[i], "otu_label": patient.otu_labels[i]};
@@ -17,18 +35,23 @@ function optionChanged(ID) {
         };
         console.log(barData);
 
+        // x and y data lists to be filled in below
         var xdata = [];
         var ydata = [];
+        // grabs all labels to be displayed on hover for that patient
         var labels = barData.map(datum => {
             return datum.otu_label;
         });
 
+        // loops through current patient's bar data, pushes data to x and y axis lists as appropriate
         for (var i=0; i < barData.length; i++) {
             currentDatum = barData[i];
             xdata.push(currentDatum.sample_value);
             ydata.push(currentDatum.otu_id);
         };
 
+        // create the bar chart data and layout once all of that has been assembled
+        // data is already sorted, so slice grabs top 10 and reverse puts most populous colonies at top of chart
         var data = [{
             type: 'bar',
             x: xdata.slice(0, 10).reverse(),
@@ -45,7 +68,7 @@ function optionChanged(ID) {
           
         Plotly.newPlot('bar', data, layout);
 
-   
+        // do the same from the bubble chart, but grab all the data
         // making the y-values from the bar chart the x-values for the bubble chart
         bubbleX = ydata
         // making the x-values from the bar chart the y-values for the bubble chart
@@ -56,6 +79,7 @@ function optionChanged(ID) {
             y: bubbleY,
             mode: 'markers',
             marker: {
+              // making the color argument an array sets the color automatically to a gradient
               color: bubbleX,
               size: bubbleY
             },
@@ -74,7 +98,9 @@ function optionChanged(ID) {
     });
 };
 
-
+// this code below runs on startup, not just when the onChange is detected.
+// displays the bubble chart and bar graph for whatever value is already populating the dropodown's field
+// (we expect it to always be the first value)
 d3
     .json("https://raw.githubusercontent.com/rose-gonoud/plotly-challenge/master/data/samples.json")
     
